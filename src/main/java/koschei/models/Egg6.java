ff72430feeee5c5453f50f95dbb5c8e1180ac6d4
Zubairov93 <zzubbairr@gmail.com>
2023-12-12 13:03:30 +0300

    добавил связывание по сеттеру

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Egg6 {
 
-    private final Needle7 needle;
+    private Needle7 needle;
 
     @Autowired
-    public Egg6(Needle7 needle) {
+    public void setNeedle(Needle7 needle) {
         this.needle = needle;
     }
+
+
 
 
 
